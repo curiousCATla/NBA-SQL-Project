d6709b85e5a9a981a874723c7a472847ed958ad8
curiousDONG <iandzy0426@gmail.com>
2026-03-27 17:32:44 -0400

    Update queries.sql

--- queries.sql
+++ queries.sql
@@ -8,7 +8,17 @@ FROM player_stats
 ORDER BY PTS DESC
 LIMIT 10;
 --Q2: Which players averaged 25+ PPG and 7+ AST in the same season?
-
+SELECT 
+Player, 
+Season, 
+Tm,
+PTS/G AS PPG, 
+AST/G AS APG
+FROM player_stats
+WHERE PPG >= 25  AND APG >= 7 AND G >= 41
+--An NBA season has 82 games, setting a minimum on the number of games played (G >= 41), filters outliers
+ORDER BY PPG DESC
+LIMIT 30
 --Q3: How has the league-wide average points per game changed each season?
 
 --Q4: Which players have averaged 20+ PPG in at least 5 different seasons?
