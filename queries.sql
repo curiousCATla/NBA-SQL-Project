73e4957e442a93f614c9e91fb6a242f45f0ad49d
curiousDONG <iandzy0426@gmail.com>
2026-03-28 13:57:50 -0400

    Update queries.sql

--- queries.sql
+++ queries.sql
@@ -21,7 +21,30 @@ ORDER BY PPG DESC
 LIMIT 30
 
 --Q3: How has the league-wide average points per game changed each season?
-SELECT
+  WITH clean_stats AS ( --The WITH notation can only be used once in a SQL
+      SELECT * FROM player_stats WHERE Tm = 'TOT'
+      UNION ALL
+      SELECT *  FROM player_stats
+      WHERE NOT EXISTS (
+          SELECT 1 FROM player_stats p2
+          WHERE p2.Player = player_stats.Player
+            AND p2.Season = player_stats.Season
+            AND p2.Tm = 'TOT'
+      )
+  ), --When a player gets traded in the middle of the season, there will be repeated row entries of the same player in their initial team, final team, and total
+	--The clean_stats CTE removes the duplicated rows and only keeps the total if a player gets traded in the season and played on both teams
+ player_avg AS ( --The second CTE does not require the WITH notation, but a "," to separate the two CTEs
+	SELECT Season, Player, ROUND(PTS/G, 2) AS PPG, G
+	FROM clean_stats
+	WHERE G >= 1
+  )
+--The second finds PPG but also filters out any null entry/ when a player did not play that season 
+SELECT 
+Season, AVG(PPG) AS avg_PPG, COUNT(*) AS player_count, AVG(G) AS avg_GP
+--In addition to average points per game, I also  
+FROM player_avg
+GROUP BY Season
+ORDER BY Season;
 
 --Q4: Which players have averaged 20+ PPG in at least 5 different seasons?
 
