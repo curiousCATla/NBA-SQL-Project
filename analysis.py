bcde068d02c18a6633adbb48d4bd8b746fedce4f
curiousDONG <iandzy0426@gmail.com>
2026-03-28 14:41:17 -0400

    Update analysis.py

--- analysis.py
+++ analysis.py
@@ -6,7 +6,6 @@ import os
 from adjustText import adjust_text
 
 conn = sqlite3.connect('nba.db')
-
 q1 = pd.read_sql_query("""
 SELECT
     Player,
@@ -17,7 +16,7 @@ FROM player_stats
 ORDER BY PTS DESC
 LIMIT 10;
 """, conn) #inputting the SQL query as a string, and the connection object to the database. The result is stored in a pandas DataFrame called q1.
-# Combine Player + Season into one label for the y-axis
+# Combine Player + Season into one label for the y-axis because players like Wilt have multiple entries on this list from different seasons 
 q1['Player_Season'] = q1['Player'] + ' (' + q1['Season'].astype(str) + ')'
 
 fig, ax = plt.subplots(figsize=(10, 6))
@@ -84,5 +83,44 @@ plt.tight_layout()
 os.makedirs('images', exist_ok=True)
 plt.savefig('images/ppg_vs_apg_scatter.png', dpi=150, bbox_inches='tight')
 plt.show()
+
+
+q3 = pd.read_sql_query("""
+  WITH clean_stats AS (
+      SELECT * FROM player_stats WHERE Tm = 'TOT'
+      UNION ALL
+      SELECT *  FROM player_stats
+      WHERE NOT EXISTS (
+          SELECT 1 FROM player_stats p2
+          WHERE p2.Player = player_stats.Player
+            AND p2.Season = player_stats.Season
+            AND p2.Tm = 'TOT'
+      )
+  ),
+ player_avg AS (
+	SELECT Season, Player, ROUND(PTS/G, 2) AS PPG, G
+	FROM clean_stats
+	WHERE G >= 1
+  )
+SELECT 
+Season, AVG(PPG) AS avg_PPG, COUNT(*) AS player_count, AVG(G) AS avg_GP
+FROM player_avg
+GROUP BY Season
+ORDER BY Season;
+""", conn) 
+
+fig, ax = plt.subplots(figsize=(12, 5))
+sns.scatterplot(
+    data=q3,
+    x='Season',
+    y='avg_PPG',
+    ax=ax
+)
+ax.set_title('Average Player Points Per Game over Time', fontsize=14)
+ax.set_xlabel('Season(year)', fontsize=12)
+ax.set_ylabel('Average Points Per Game', fontsize=12)
+
+plt.tight_layout()
+plt.savefig('images/avg_ppg_by_season.png', dpi=150, bbox_inches='tight')
 
 conn.close()
